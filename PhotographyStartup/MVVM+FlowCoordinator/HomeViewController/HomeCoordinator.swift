7719bd5ee0b8ac89160b89eec18d621cc76f9cfe
Dmitrii Lemaikin <Dmitrii_Lemaikin@epam.com>
2018-09-20 18:33:04 +0300

    Added HomeViewModel

---
+++ PhotographyStartup/MVVM+FlowCoordinator/HomeViewController/HomeCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  HomeCoordinator.swift
+//  PhotographyStartup
+//
+//  Created by Dmitry Lemaykin on 9/20/18.
+//  Copyright © 2018 Dmitry Lemaykin. All rights reserved.
+//
+
+import Foundation
